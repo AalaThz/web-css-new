6988c3615f0ebff1d70b255efc12cfb45103aa50
AalaThz <117166582+AalaThz@users.noreply.github.com>
2023-05-02 09:42:25 +0330

    Add files via upload

---
+++ scripts/shared.js
@@ -0,0 +1,19 @@
+let backdrop = document.querySelector('.backdrop')
+let modal = document.querySelector('.modal')
+let selectPlanBtn = document.querySelectorAll('.plan button')
+let modalCloseBtn = document.querySelector('.modal__action--negative')
+
+for (i=0 ; i<selectPlanBtn.length ; i++) {
+    selectPlanBtn[i].addEventListener('click', () => {
+        modal.style.display = 'block'
+        backdrop.style.display = 'block'
+    })
+}
+backdrop.addEventListener('click',closeModal )
+modalCloseBtn.addEventListener('click',closeModal )
+
+
+function closeModal (){
+    modal.style.display = 'none'
+    backdrop.style.display = 'none'
+}
